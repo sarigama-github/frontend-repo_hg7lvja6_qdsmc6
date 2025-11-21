8a995179f0b3e01cd91e9c52b4657cba0c1bde46
Flames <vibe-coder@flames.com>
2025-11-21 13:10:09 +0000

    Update project prj_hg7lvja6 - 2025-11-21T13:10:08.085Z

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import Test from './Test'
-import ErrorBoundary from './components/ErrorBoundary'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ErrorBoundary>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/test" element={<Test />} />
-      </Routes>
-    </BrowserRouter>
-  </ErrorBoundary>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/test" element={<Test />} />
+    </Routes>
+  </BrowserRouter>,
 )
